b3765e6a23a9332772e621d3e24b79885d7e30e4
RamiroVT <u202123714@upc.edu.pe>
2024-06-08 21:32:58 -0500

    add env support

--- src/main/java/org/Arquitech/Gymrat/configserver/ConfigServerApplication.java
+++ src/main/java/org/Arquitech/Gymrat/configserver/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package org.Arquitech.Gymrat.configserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,9 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
 }
